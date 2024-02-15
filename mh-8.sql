c39dca71dfe6edb78c5d0a147a401ed5a4e466f8
cagatayozsiray <137600469+cagatayozsiray@users.noreply.github.com>
2024-02-15 21:33:21 +0300

    Add files via upload

---
+++ mh-8.sql
@@ -0,0 +1,24 @@
+
+
+SELECT 
+CouncilArea,
+     ISNULL([Northern Victoria], 0) AS [Northern Victoria],
+           ISNULL([South-Eastern Metropolitan], 0) AS [South-Eastern Metropolitan],
+           ISNULL([Western Victoria], 0) AS [Western Victoria],
+           ISNULL([Eastern Victoria], 0) AS [Eastern Victoria],
+           ISNULL([Western Metropolitan], 0) AS [Western Metropolitan],
+           ISNULL([Southern Metropolitan], 0) AS [Southern Metropolitan],
+           ISNULL([Northern Metropolitan], 0) AS [Northern Metropolitan],
+           ISNULL([Eastern Metropolitan], 0) AS [Eastern Metropolitan]
+FROM (
+    SELECT CouncilArea, Regionname, COUNT(*) AS Sale_Count
+    FROM melbourne_housing
+    GROUP BY CouncilArea, Regionname
+) AS SourceTable
+PIVOT (
+    SUM(Sale_Count)
+    FOR Regionname IN ([Northern Victoria],[South-Eastern Metropolitan],[Western Victoria],[Eastern Victoria],
+         [Western Metropolitan],[Southern Metropolitan], [Northern Metropolitan],[Eastern Metropolitan])
+) AS PivotTable
+ORDER BY CouncilArea
+
